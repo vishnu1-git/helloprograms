47ad10c30aec7044239cab592bc8c63a0c4dd8c8
vishnu1-git <kanukuvishnu1@gmail.com>
2026-01-30 14:48:43 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <h1 id="greeting"></h1>
+
+    <script>
+        document.getElementById("greeting").innerText = "Hello, World!";
+    </script>
+</body>
+</html>
