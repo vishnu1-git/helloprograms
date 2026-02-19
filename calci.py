afc60a89adfeb2763520daa60e47dba27cf873f5
vishnu1-git <kanukuvishnu1@gmail.com>
2026-02-19 09:48:59 +0530

    Create calci.py
    CALCI PROG IN PYTHoN

---
+++ calci.py
@@ -0,0 +1,13 @@
+import sys
+n1 = int(sys.argv[1])
+n2 = int(sys.argv[2])
+r1 = n1+n2
+r2 = n1-n2
+r3 = n1*n2
+r4 = n1/n2
+print("Addition",r1)
+print("Subtraction",r2)
+print("Multiplication",r3)
+print("Division",r4)
+print("Changes are made in python file")
+print("Changes are made in python file on wednesday at 3:35pm")
